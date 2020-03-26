047ad49c365de6045f132edf377765d01cd792ed
CiaraMaria Proia <proiacm@gmail.com>
2020-03-26 21:47:01 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == 
+      :suite_a && name.start_with?("A")
+      winnter = name
+    end
 end
+winner
